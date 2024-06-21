08a1edc75a25dfeb443998b8d68d9707c60c38b0
RichardHerna <20220194@ricaldone.edu.sv>
2024-06-21 09:09:25 -0600

    pantalla presnetacion lista

--- pantallas/presentaciones.js
+++ pantallas/presentaciones.js
@@ -1,18 +1,133 @@
-import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { Searchbar, Card, Title, Paragraph, Modal, Portal, Text, Button, TextInput, Provider } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Presentaciones = () => {
-    return(
-    < View >
-        <Text
-            style={{
-                fontSize: 30,
-                textAlign: "center",
-                marginTop: "20%"
-            }}
-        >Presentaciones</Text>
-    </View >
-    );
-}
+const presentacionesPrueba = [
+    { id: '1', presentation: 'Caja' },
+    { id: '2', presentation: 'Caja' },
+    { id: '3', presentation: 'Arroba' },
+    { id: '4', presentation: 'Caja' },
+    { id: '5', presentation: 'Fardo'},
+];
 
-export default Presentaciones;
+export default function Presentaciones() {
+        const [presentations, setPresentations] = useState(presentacionesPrueba);
+        const [searchQuery, setSearchQuery] = useState('');
+        const [visible, setVisible] = useState(false);
+        const [newPresentation, setPresentation] = useState({
+            presentation: '',
+        });
+    
+        const onChangeSearch = query => setSearchQuery(query);
+    
+        const filteredPresentations = presentations.filter(presentation =>
+            presentation.presentation.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    
+        const renderItem = ({ item }) => (
+            <Card style={styles.card}>
+                <Card.Content>
+                    <Title>{item.presentation}</Title>
+                    <Paragraph>Presentacion: {item.presentation}</Paragraph>
+                </Card.Content>
+            </Card>
+        );
+    
+        const showModal = () => setVisible(true);
+        const hideModal = () => setVisible(false);
+    
+        const handleInputChange = (presentation, value) => {
+            setPresentation({ ...newPresentation, [presentation]: value });
+        };
+    
+        // const addProduct = () => {
+        //     setProducts([...products, { ...newProduct, id: (products.length + 1).toString() }]);
+        //     setNewProduct({
+        //         name: '',
+        //         expiration: '',
+        //         description: '',
+        //         stock: '',
+        //         category: '',
+        //         brand: '',
+        //         presentation: '',
+        //     });
+        //     hideModal();
+        // };
+    
+        return (
+            <Provider>
+                <View style={styles.container}>
+                    <Searchbar
+                        placeholder="Buscar Presentacion"
+                        onChangeText={onChangeSearch}
+                        value={searchQuery}
+                        style={styles.searchbar}
+                    />
+                    <Button onPress={showModal} style={styles.addButton}>
+                    <MaterialCommunityIcons name="plus-circle-outline" size={22} color="#9368EE" />
+                    </Button>
+                    <FlatList
+                        data={filteredPresentations}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                    />
+                    <Portal>
+                        <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.modalContainer}>
+                            <Button onPress={hideModal} style={styles.closeButton}>
+                                <MaterialCommunityIcons name="close-thick" size={20} color="red" />
+                            </Button>
+                            <TextInput
+                                label="Presentacion"
+                                value={newPresentation.presentation}
+                                onChangeText={text => handleInputChange('presentation', text)}
+                                style={styles.input}
+                            />
+                            <Button mode="contained" style={styles.saveButton}>
+                                Guardar
+                            </Button>
+                        </Modal>
+                    </Portal>
+                </View>
+            </Provider>
+        );
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            paddingTop: 20,
+            paddingHorizontal: 10,
+        },
+        searchbar: {
+            marginBottom: 10,
+        },
+        card: {
+            marginBottom: 10,
+        },
+        addButton: {
+            marginTop: 5,
+            alignSelf: "flex-end",
+        },
+        modalContainer: {
+            backgroundColor: 'white',
+            padding: 20,
+            margin: 20,
+        },
+        input: {
+            marginBottom: 10,
+        },
+        saveButton: {
+            marginTop: 5,
+            backgroundColor: 'blue',
+            width: 150,
+            alignSelf: "flex-end",
+        },
+        closeButton: {
+            marginTop: 10,
+            width: 5,
+            alignSelf: "flex-end",
+            marginBottom: 10
+        },
+    });
+    
