5b2f39446a3b83c21873843cf89d1e8400538191
GerardoTamayo <gtr012705@gmail.com>
2024-08-07 18:44:10 -0600

    productos leidos desde base de datos

--- src/pantallas/productos.js
+++ src/pantallas/productos.js
@@ -1,6 +1,7 @@
 // Importamos React y useState para poder utilizar estados en el componente.
-import React, { useState } from 'react';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import * as Constantes from '../utilidades/constante';
 // Importamos los componentes necesarios de react-native y react-native-paper.
 import { View, FlatList, StyleSheet } from 'react-native';
 import { Searchbar, Card, Title, Paragraph, Modal, Portal, Text, Button, TextInput, Provider } from 'react-native-paper';
@@ -8,61 +9,88 @@ import { Searchbar, Card, Title, Paragraph, Modal, Portal, Text, Button, TextInp
 // Importamos los iconos de MaterialCommunityIcons de @expo/vector-icons.
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-// Definimos una lista de productos de prueba.
-const productosPrueba = [
-    { id: '1', name: 'Cereal del zucarita', expiration: '17/11/2025', stock: 25, brand: 'zucarita' },
-    { id: '2', name: 'Cereal del zucarita', expiration: '17/11/2025', stock: 25, brand: 'zucarita' },
-    { id: '3', name: 'Cereal del zucarita', expiration: '17/11/2025', stock: 25, brand: 'zucarita' },
-    { id: '4', name: 'Cereal del zucarita', expiration: '17/11/2025', stock: 25, brand: 'zucarita' },
-    { id: '5', name: 'Cereal del zucarita', expiration: '17/11/2025', stock: 25, brand: 'zucarita' },
-];
 
 // Definimos el componente principal de Productos.
 export default function Productos() {
+    const ip = Constantes.IP;
     // Definimos los estados necesarios.
-    const [products, setProducts] = useState(productosPrueba);
     const [searchQuery, setSearchQuery] = useState('');
     const [visible, setVisible] = useState(false);
-    const [newProduct, setNewProduct] = useState({
-        name: '',
-        expiration: '',
-        description: '',
-        stock: '',
-        category: '',
-        brand: '',
-        presentation: '',
-    });
+    const [deleteDialogVisible, setDeleteDialogVisible] = useState(false);
+    const [idToDelete, setIdToDelete] = useState(null);
+    const [idToUpdate, setIdToUpdate] = useState(null);
 
-    // Función que maneja el cambio en el campo de búsqueda.
-    const onChangeSearch = query => setSearchQuery(query);
+    // Función para mostrar el modal.
+    const showModal = () => setVisible(true);
+    // Función para ocultar el modal.
+    const hideModal = () => {
+        setVisible(false);
+        setIdToUpdate(null); // Restablecer idToUpdate a null cuando se cierra el modal
+        limpiarCampos();
+    }
 
-    // Filtramos los productos según la consulta de búsqueda.
-    const filteredProducts = products.filter(product =>
-        product.name.toLowerCase().includes(searchQuery.toLowerCase())
+    // Estados para manejar los datos, carga y errores
+    const [response, setResponse] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [nombre, setNombre] = useState('');
+    const [Fecha, setFecha] = useState('');
+    const [Descripcion, setDescripcion] = useState('');
+    const [Existencias, setExistencias] = useState('');
+    const [Categoria, setCategoria] = useState('');
+    const [Marca, setMarca] = useState('');
+    const [Presentacion, setPresentacion] = useState('');
+    const [error, setError] = useState(null);
+
+    // Limpiar campos del formulario
+    const limpiarCampos = async () => {
+        setNombre('');
+        setFecha('');
+        setDescripcion('');
+        setExistencias('');
+        setCategoria('');
+        setMarca('');
+        setPresentacion('');
+    };
+
+    // Función para obtener datos de la API
+    const fillList = async () => {
+        try {
+            // Realiza una solicitud GET para verificar la sesión del usuario
+            const response = await fetch(`${ip}/Expo2024/expo/api/servicios/administrador/producto.php?action=readAll`, {
+                method: 'GET'
+            });
+            const data = await response.json();
+            setResponse(data.dataset)
+            console.log(data)
+        } catch (error) {
+            console.error(error);
+            Alert.alert('Error');
+        }
+    };
+
+    // Ejecuta fillList al montar el componente
+    useEffect(() => {
+        fillList();
+    }, []);
+
+    // Ejecuta fillList cuando la pantalla recibe foco
+    useFocusEffect(
+        useCallback(() => {
+            fillList();
+        }, [])
     );
 
     // Función que renderiza cada ítem en la lista.
     const renderItem = ({ item }) => (
         <Card style={styles.card}>
             <Card.Content>
-                <Title>{item.name}</Title>
-                <Paragraph>Vencimiento: {item.expiration}</Paragraph>
-                <Paragraph>Existencias: {item.stock}</Paragraph>
-                <Paragraph>Marca: {item.brand}</Paragraph>
+                <Title>Producto: {item.nombre_producto}</Title>
+                <Paragraph>Vencimiento: {item.fecha_vencimiento}</Paragraph>
+                <Paragraph>Existencias: {item.existencias_producto}</Paragraph>
+                <Paragraph>Marca: {item.nombre_marca}</Paragraph>
             </Card.Content>
         </Card>
     );
-
-    // Función para mostrar el modal.
-    const showModal = () => setVisible(true);
-
-    // Función para ocultar el modal.
-    const hideModal = () => setVisible(false);
-
-    // Función que maneja el cambio de texto en el campo del nuevo producto.
-    const handleInputChange = (name, value) => {
-        setNewProduct({ ...newProduct, [name]: value });
-    };
 
     // Retornamos el componente.
     return (
@@ -71,7 +99,7 @@ export default function Productos() {
                 {/* Barra de búsqueda */}
                 <Searchbar
                     placeholder="Buscar producto"
-                    onChangeText={onChangeSearch}
+                    // onChangeText={onChangeSearch}
                     value={searchQuery}
                     style={styles.searchbar}
                 />
@@ -79,11 +107,10 @@ export default function Productos() {
                 <Button onPress={showModal} style={styles.addButton}>
                     <MaterialCommunityIcons name="plus-circle-outline" size={22} color="#9368EE" />
                 </Button>
-                {/* Lista de productos filtrados */}
                 <FlatList
-                    data={filteredProducts}
+                    data={response}
                     renderItem={renderItem}
-                    keyExtractor={item => item.id}
+                    keyExtractor={item => item.id_producto.toString()}
                 />
                 {/* Modal para añadir nuevo producto */}
                 <Portal>
@@ -93,43 +120,43 @@ export default function Productos() {
                         </Button>
                         <TextInput
                             label="Nombre"
-                            value={newProduct.name}
+                            // value={newProduct.name}
                             onChangeText={text => handleInputChange('name', text)}
                             style={styles.input}
                         />
                         <TextInput
                             label="Fecha vencimiento"
-                            value={newProduct.expiration}
+                            // value={newProduct.expiration}
                             onChangeText={text => handleInputChange('expiration', text)}
                             style={styles.input}
                         />
                         <TextInput
                             label="Descripción"
-                            value={newProduct.description}
+                            // value={newProduct.description}
                             onChangeText={text => handleInputChange('description', text)}
                             style={styles.input}
                         />
                         <TextInput
                             label="Existencias"
-                            value={newProduct.stock}
+                            // value={newProduct.stock}
                             onChangeText={text => handleInputChange('stock', text)}
                             style={styles.input}
                         />
                         <TextInput
                             label="Categoría"
-                            value={newProduct.category}
+                            // value={newProduct.category}
                             onChangeText={text => handleInputChange('category', text)}
                             style={styles.input}
                         />
                         <TextInput
                             label="Marca"
-                            value={newProduct.brand}
+                            // value={newProduct.brand}
                             onChangeText={text => handleInputChange('brand', text)}
                             style={styles.input}
                         />
                         <TextInput
                             label="Presentación"
-                            value={newProduct.presentation}
+                            // value={newProduct.presentation}
                             onChangeText={text => handleInputChange('presentation', text)}
                             style={styles.input}
                         />
